323c73d1eecb4e122bbdc10dd1f874bbc0f39923
Loïc Goyet <loic@appventus.com>
2017-01-02 17:15:35 +0100

    basic click interaction

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // Logic (functional)
-function main() {
+function main(DOMSource) {
+  const click$ = DOMSource;
+
   return {
-    DOM: Rx.Observable.timer(0, 1000).map(i => `Seconds elapsed ${i}`),
+    DOM: click$
+      .startWith(null)
+      .flatMapLatest(() => Rx.Observable.timer(0, 1000).map(i => `Seconds elapsed ${i}`)),
     Log: Rx.Observable.timer(0, 2000).map(i => 2 * i),
   };
 }
@@ -12,6 +16,9 @@ function DOMDriver(text$) {
     const container = document.querySelector('#app');
     container.textContent = text;
   });
+
+  const DOMSource = Rx.Observable.fromEvent(document, 'click');
+  return DOMSource;
 }
 
 function consoleLogDriver(msg$) {
@@ -19,10 +26,14 @@ function consoleLogDriver(msg$) {
 }
 
 function run(mainFn, drivers) {
-  const sinks = mainFn();
-  Object.keys(drivers).forEach(key => {
-    drivers[key](sinks[key]);
-  });
+  const proxyDOMSource = new Rx.Subject();
+  const sinks = mainFn(proxyDOMSource);
+  const DOMSource = drivers.DOM(sinks.DOM);
+  DOMSource.subscribe(click => proxyDOMSource.onNext(click));
+
+  // Object.keys(drivers).forEach(key => {
+  //   drivers[key](sinks[key]);
+  // });
 }
 
 const driversFunctions = {
