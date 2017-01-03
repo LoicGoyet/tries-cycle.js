c5447ee08c0dbceb958f4105a907ccde6a2bbff2
Loïc Goyet <loic@appventus.com>
2017-01-03 10:10:45 +0100

    From Toy DOM Driver to real DOM Driver

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-function h(tagName, children) {
-  return {
-    tagName: tagName,
-    children: children,
-  }
-}
-
-function h1(children) {
-  return h('H1', children);
-}
-
-function span(children) {
-  return h('SPAN', children);
-}
+const {h, h1, span, makeDOMDriver} = CycleDOM;
 
 // Logic (functional)
 function main(sources) {
-  const mouseover$ = sources.DOM.selectEvents('span', 'mouseover');
+  const mouseover$ = sources.DOM.select('span').events('mouseover');
 
   const sinks = {
     DOM: mouseover$
@@ -36,44 +23,13 @@ function main(sources) {
 }
 
 // Drivers (imperative)
-function DOMDriver(obj$) {
-  function createElement(obj) {
-    const element = document.createElement(obj.tagName);
-
-    obj.children
-      .filter(c => typeof c === 'object')
-      .map(createElement)
-      .forEach(c => element.appendChild(c));
-
-    obj.children
-      .filter(c => typeof c === 'string')
-      .forEach(c => element.innerHTML += c);
-
-    return element;
-  }
-
-  obj$.subscribe(obj => {
-    const container = document.querySelector('#app');
-    container.innerHTML = '';
-    const element = createElement(obj);
-    container.appendChild(element);
-  });
-
-  const DOMSource = {
-    selectEvents: function(tagName, eventType) {
-      return Rx.Observable.fromEvent(document, eventType)
-        .filter(ev => ev.target.tagName === tagName.toUpperCase());
-    }
-  };
-  return DOMSource;
-}
 
 function consoleLogDriver(msg$) {
   msg$.subscribe(msg => console.log(msg));
 }
 
 const driversFunctions = {
-  DOM: DOMDriver,
+  DOM: makeDOMDriver('#app'),
   Log: consoleLogDriver,
 }
 
